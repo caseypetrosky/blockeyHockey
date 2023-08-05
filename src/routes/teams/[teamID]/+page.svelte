58b457b0fc0f518166a2c4a70831afcb4ee878aa
Casey Petrosky <caseypetrosky16@gmail.com>
2023-08-05 11:14:43 -0500

    Teams updates

--- src/routes/teams/[teamID]/+page.svelte
+++ src/routes/teams/[teamID]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+	import CutePlayerDisp from '$lib/cutePlayerDisp.svelte';
     import {page} from '$app/stores';
     export let data;
     let teamImg = `/src/static/logos/${data.team.id}.png`;
+    let rinkImg = `/src/static/logos/${data.team.id}rink.png`
+    let teamPlayers = data.players;
     let teamColor = data.team.color;
 </script>
 
@@ -11,20 +14,44 @@
     <div class="inline-flex">
         <img src={teamImg} alt="team logo" class="h-40 w-40 mr-4">
         <div>
-            <h1 class="my-auto text-4xl text-[{teamColor}]">{data.team.name}</h1>
+            <h1 class="my-auto text-4xl">{data.team.name}</h1>
             <h2>Owner: {data.team.team_owner}</h2>
-            <h2>Leauge: {data.league.name}</h2>
+            <h2>League: {data.league.name}</h2>
             {#if data.team.championships   != null  }
                 <h3>Championships: {data.team.championships}</h3>
             {/if}
 
         </div>
-
     </div>  
+    <h2 class="text-2xl mt-4">Description</h2>
+    <hr class=" border-2 w-1/4" style="border-color: {teamColor}">
     <p>{data.team.description}</p>
-</div>
+    <h2 class="text-2xl mt-4">Roster</h2>
+    <hr class=" border-2 w-1/4" style="border-color: {teamColor}">
+    <div class="grid lg:grid-cols-10 md:grid-cols-5 sm:grid-cols-5 grid-cols-3">
+    {#if teamPlayers.length > 0}
+        {#each teamPlayers as player}
+              <CutePlayerDisp player = {player}/>
+              <CutePlayerDisp player = {player}/>
+              <CutePlayerDisp player = {player}/>
+              <CutePlayerDisp player = {player}/>
+              <CutePlayerDisp player = {player}/>
+
+
+        {/each}
+    {:else}
+              <h1>This team has no players</h1>
+    {/if}
+        </div>
+   
+    <h2 class="text-2xl mt-4">Stadium</h2>
+    <hr class="border-2 w-1/4" style="border-color: {teamColor}">
+    
+</div>  
 <!--
+<img src={rinkImg}>
+
+-->
 <pre>
     {JSON.stringify($page,null,2)}
 </pre>  
--->
