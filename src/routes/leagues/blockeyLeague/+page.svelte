86f77b9c13d269eb92ab76405437db41a2f491f6
Casey Petrosky <caseypetrosky16@gmail.com>
2023-08-29 20:11:51 -0500

    Previous BHL Seasons

--- src/routes/leagues/blockeyLeague/+page.svelte
+++ src/routes/leagues/blockeyLeague/+page.svelte
@@ -31,7 +31,120 @@
       <h2 class="text-2xl font-bold mb-4">Schedule</h2>
       <p class="text-lg">Stay up to date with the latest matches and game schedules in the Blockey Hockey League. All scheduled games and reschedules can be found in the discord servers.</p>
     </section>
+    <section class="mt-8">
+      <h2 class="text-3xl font-bold">Previous Seasons</h2>
+     
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+      <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 1</h1>
+        <p class="text-lg">Season 1 Champion - <strong>San Francisco Flight</strong> <br>
+        3-2 series win vs Hollywood Hattricks <br>
+        Conn Smythe Winner: <strong>DNP_10</strong> </p>
+      </div>
+      <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 2</h1>
+        <p class="text-lg">Season 2 Champion - <strong>Hartford Whalers</strong> <br>
+        3-1 series win vs Vancouver Orcas<br>
+        Conn Smythe Winner: <strong>Oilerator</strong> </p>
+      </div>
+      <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 3</h1>
+        <p class="text-lg">Season 3 Champion - <strong>Indianapolis Racer</strong> <br>
+        3-1 series win vs Hartford Whalers<br>
+        Conn Smythe Winner: <strong>EtheD99</strong> </p>
+      </div>
+      <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 4</h1>
+        <p class="text-lg">Season 4 Champion - <strong>Indianapolis Racer</strong> <br>
+        3-0 series win vs Hartford Whalers<br>
+        Conn Smythe Winner: <strong>Charn__</strong> </p>
+    </div>
+    <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 5</h1>
+        <p class="text-lg">Season 5 Champion - <strong>Michigan Hornets</strong> <br>
+        3-0 series win vs Shenandoah Scorched<br>
+        Conn Smythe Winner: <strong>blackus_knightus</strong> </p>
+    </div>
+    <div>
+        <h1 class="text-2xl font-bold mb-4 mt-8">Season 6</h1>
+        <p class="text-lg">Season 6 Champion - <strong>Michigan Hornets</strong> <br>
+        3-0 series win vs Hollywood Hattricks<br>
+        Conn Smythe Winner: <strong>Jam0828</strong> </p>
+    </div>
+    <div>
+      <h1 class="text-2xl font-bold mb-4 mt-8">Season 7</h1>
+      <p class="text-lg">Season 7 Champion - <strong><a class="hover:text-gray-400" href="../teams/BOS">Nova Scotia Huskies</a></strong> <br>
+      3-1 series win vs Fort Worth jets<br>
+      Conn Smythe Winner: <strong>Youppi</strong> </p>
   </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 8</h1>
+      <p class="text-lg">Season 8 Champion - <strong>Carolina Cobras</strong> <br>
+      3-1 series win vs Nova Scotia Huskies<br>
+      Conn Smythe Winner: <strong>IsedTheDoggo</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 9</h1>
+      <p class="text-lg">Season 9 Champion - <strong><a class="hover:text-gray-400" href="../teams/HOU">Houston Bandits</a></strong> <br>
+      3-2 series win vs Nova Scotia Huskies<br>
+      Conn Smythe Winner: <strong>ohLuvv</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 10</h1>
+      <p class="text-lg">Season 10 Champion - <strong><a class="hover:text-gray-400" href="../teams/BOS"> Nova Scotia Huskies</a></strong> <br>
+      3-0 series win vs Seattle Golden Eagles<br>
+      Conn Smythe Winner: <strong>Youppi</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 11</h1>
+      <p class="text-lg">Season 11 Champion - <strong><a class="hover:text-gray-400" href="../teams/BOS"> Nova Scotia Huskies</a></strong> <br>
+      3-2 series win vs Orlando Storm<br>
+      Conn Smythe Winner: <strong>Emener</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 12</h1>
+      <p class="text-lg">Season 12 Champion - <strong>Seattle Golden Eagles</strong> <br>
+      3-0 series win vs Surrey Hitmen<br>
+      Conn Smythe Winner: <strong>Youppi</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 13</h1>
+      <p class="text-lg">Season 13 Champion - <strong>Halifax Highlanders</strong> <br>
+      3-0 series win vs Surrey Hitmen<br>
+      Conn Smythe Winner: <strong>Robby_Ruckus</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 14</h1>
+      <p class="text-lg">Season 14 Champion - <strong>Halifax Highlanders</strong> <br>
+      3-1 series win vs Surrey Hitmen<br>
+      Conn Smythe Winner: <strong>Youppi</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 15</h1>
+      <p class="text-lg">Season 15 Champion - <strong>San Fransico Stingrays</strong> <br>
+        3-0 series win over Quebec City Voyageurs<br>
+      Conn Smythe Winner: <strong>apat8</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 16</h1>
+      <p class="text-lg">Season 16 Champion - <strong><a class="hover:text-gray-400" href="../teams/BOS">Boston Grizzlies</a></strong> <br>
+        3-0 series win over Saskatoon blues<br>
+      Conn Smythe Winner: <strong>rdoggy</strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 17</h1>
+      <p class="text-lg">Season 17 Champion - <strong><a class="hover:text-gray-400" href="../teams/BOS">Boston Grizzlies</a></strong> <br>
+        3-0 series win over Halifax Highlanders<br>
+      Conn Smythe Winner: <strong>Vasilevskiy </strong> </p>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4 mt-8">Season 18</h1>
+      <p class="text-lg">Season 18 Champion - <strong>Vancouver Outlaws</strong> <br>
+        3-2 series win over Boston Grizzlies<br>
+      Conn Smythe Winner: <strong>Annoy_d</strong> </p>
+  </section>
+  </div>
+
 
 
 
